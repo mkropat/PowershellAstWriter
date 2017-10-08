c10417ab10a855036730978978e9b4405ee95138
Michael Kropat <mail@michael.kropat.name>
2017-10-08 14:01:47 -0400

    Checkpoint

---
+++ PowershellAstWriter/PowershellAstWriter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation.Language;
+
+namespace PowershellAstWriter
+{
+    public class PowershellAstWriter
+    {
+        public string Write(ScriptBlockAst ast) {
+            if (!ast.EndBlock.Statements.Any())
+            {
+                return string.Empty;
+            }
+            var pipe = ast.EndBlock.Statements.OfType<PipelineAst>().First();
+
+            var cmd = pipe.PipelineElements.First();
+            var cmdCmd = cmd as CommandAst;
+            var cmdExpression = cmd as CommandExpressionAst;
+
+            if (cmdCmd != null)
+            {
+                var prefix = "";
+                if (cmdCmd.InvocationOperator != TokenKind.Unknown)
+                {
+                    prefix = TranslateToken(cmdCmd.InvocationOperator) + " ";
+                }
+                return prefix + TranslateExpression(cmdCmd.CommandElements.First());
+            }
+            else if (cmdExpression != null)
+            {
+                return TranslateExpression(cmdExpression.Expression);
+            }
+            else
+            {
+                throw new Exception("unhandled");
+            }
+        }
+
+        private string TranslateToken(TokenKind token)
+        {
+            switch (token)
+            {
+                case TokenKind.Ampersand:
+                    return "&";
+                default:
+                    throw new Exception("unhandled");
+            }
+        }
+
+        private string TranslateExpression(object expression)
+        {
+            var type = expression.GetType();
+            if (type == typeof(StringConstantExpressionAst))
+            {
+                var constant = (StringConstantExpressionAst)expression;
+                switch (constant.StringConstantType)
+                {
+                    case StringConstantType.BareWord:
+                        return constant.Value;
+                    case StringConstantType.DoubleQuoted:
+                        return '"' + constant.Value + '"';
+                    case StringConstantType.SingleQuoted:
+                        return '\'' + constant.Value + '\'';
+                    default:
+                        throw new Exception("unhandled");
+                }
+            }
+            else if (type == typeof(ConstantExpressionAst))
+            {
+                var constant = (ConstantExpressionAst)expression;
+                return constant.Value.ToString();
+            }
+            else if (type == typeof(VariableExpressionAst))
+            {
+                var var = (VariableExpressionAst)expression;
+                return '$' + var.VariablePath.UserPath;
+            }
+            else
+            {
+                throw new Exception("unhandled");
+            }
+        }
+    }
+}
---
+++ PowershellAstWriterTests/RoundTripTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Management.Automation.Language;
+
+namespace PowershellAstWriterTests
+{
+    public class RoundTripTests
+    {
+        [TestCase("", "")]
+        [TestCase("42", "42")]
+        [TestCase("\"derp\"", "\"derp\"")]
+        [TestCase("'derp'", "'derp'")]
+        public void ItRoundTripsConstants(string code, string expected)
+        {
+            RoundTrip(code, expected);
+        }
+
+        [TestCase("$derp", "$derp")]
+        public void ItRoundTripsVariables(string code, string expected)
+        {
+            RoundTrip(code, expected);
+        }
+
+        [TestCase("Invoke-SomeCmdlet", "Invoke-SomeCmdlet")]
+        [TestCase("& Invoke-SomeCmdlet", "& Invoke-SomeCmdlet")]
+        public void ItRoundTripsInvocations(string code, string expected)
+        {
+            RoundTrip(code, expected);
+        }
+
+        void RoundTrip(string code, string expected)
+        {
+            var ast = Parser.ParseInput(code, out Token[] tokens, out ParseError[] errors);
+            var subject = new PowershellAstWriter.PowershellAstWriter();
+            Assert.AreEqual(expected, subject.Write(ast));
+        }
+    }
+}
