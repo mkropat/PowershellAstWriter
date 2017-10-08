9355bf5dd80969dad1f0b738cfbccafda823797d
Michael Kropat <mail@michael.kropat.name>
2017-10-08 12:06:24 -0400

    Initial commit

---
+++ PowershellAstWriterTests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace PowershellAstWriterTests
+{
+    public class Class1
+    {
+        [Test]
+        public void ItRuns()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
